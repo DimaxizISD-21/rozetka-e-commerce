bcbdfb61dec2fe8e3b730eda9259eab76b3b605e
Dimaxiz <alosha.nikolanko333@gmail.com>
2021-01-28 15:54:39 +0200

    fix bugs

--- rozetka_project/settings/production_settings.py
+++ rozetka_project/settings/production_settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rozetka-e-commerce.herokuapp.com']
 
 
 # Application definition
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
